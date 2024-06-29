3c19e6cb379d5458cff898a4c4cb31b91bf42944
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-06-29 20:03:01 +0530

    Made admin layout only accessible for admin users

--- src/layouts/admin.layout.jsx
+++ src/layouts/admin.layout.jsx
@@ -1,7 +1,16 @@
+import Spinner from "@/components/shared/Spinner";
 import { Button } from "@/components/ui/button";
-import { Link, Outlet } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+import { Link, Navigate, Outlet } from "react-router-dom";
 
 function AdminLayout() {
+  const { user, isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return <Spinner />;
+
+  if (!isSignedIn || user?.publicMetadata?.role !== "admin")
+    return <Navigate to="/sign-in" />;
+
   return (
     <div>
       <div className="flex justify-end items-center gap-x-4 py-4">
